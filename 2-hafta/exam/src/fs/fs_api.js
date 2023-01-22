a45a77edfdf80b093b3bab8a99d6e0e91e2ccb2e
Kamoliddin <kamoliddinabduraimov@gmail.com>
2023-01-22 15:47:27 +0500

    Updated GET method, added checking by by existtin  id

--- 2-hafta/exam/src/fs/fs_api.js
+++ 2-hafta/exam/src/fs/fs_api.js
@@ -41,12 +41,23 @@ function get_markets(res, id) {
   });
 
   if (id) {
+    let idBool = false;
+    let rez;
+
     markets.forEach((market) => {
       if (market.marketId == id) {
-        res.writeHead(200, { "Content-Type": "application/json" });
-        return res.end(JSON.stringify(market, null, 2));
+        idBool = true;
+        rez = market;
       }
     });
+
+    if (idBool) {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      return res.end(JSON.stringify(rez, null, 2));
+    } else {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      return res.end(`marketId ${id} is not founded!!!`);
+    }
   } else {
     res.writeHead(200, { "Content-Type": "application/json" });
     return res.end(JSON.stringify(markets, null, 2));
@@ -54,19 +65,18 @@ function get_markets(res, id) {
 }
 
 function get_branches(res, id) {
-
   let branches = file_read("branches.json");
   let workers = file_read("workers.json");
   let products = file_read("products.json");
+
   branches.forEach((branch) => {
-      branch["workers"] = [];
+    branch["workers"] = [];
     workers.forEach((worker) => {
       if (worker.branchId == branch.branchId) {
         delete worker.branchId;
         branch.workers.push(worker);
       }
     });
-    
 
     branch["products"] = [];
     products.forEach((product) => {
@@ -74,16 +84,27 @@ function get_branches(res, id) {
         delete product.branchId;
         branch.products.push(product);
       }
-    });    
+    });
   });
 
   if (id) {
+    let idBool = false;
+    let rez;
+
     branches.forEach((branch) => {
       if (branch.branchId == id) {
-        res.writeHead(200, { "Content-Type": "application/json" });
-        return res.end(JSON.stringify(branch, null, 2));
+        idBool = true;
+        rez = branch;
       }
     });
+
+    if (idBool) {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      return res.end(JSON.stringify(rez, null, 2));
+    } else {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      return res.end(`BranchId ${id} is not founded!!!`);
+    }
   } else {
     res.writeHead(200, { "Content-Type": "application/json" });
     return res.end(JSON.stringify(branches, null, 2));
@@ -91,16 +112,25 @@ function get_branches(res, id) {
 }
 
 function get_products(res, id) {
-
   let products = file_read("products.json");
 
   if (id) {
+    let idBool = false;
+    let rez;
+
     products.forEach((product) => {
       if (product.productId == id) {
-        res.writeHead(200, { "Content-Type": "application/json" });
-        return res.end(JSON.stringify(product, null, 2));
+        idBool = true;
+        rez = product;
       }
     });
+    if (idBool) {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      return res.end(JSON.stringify(rez, null, 2));
+    } else {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      return res.end(`ProductId ${id} is not founded!!!`);
+    }
   } else {
     res.writeHead(200, { "Content-Type": "application/json" });
     return res.end(JSON.stringify(products, null, 2));
@@ -108,14 +138,24 @@ function get_products(res, id) {
 }
 
 function get_workers(res, id) {
-
   let workers = file_read("workers.json");
 
   if (id) {
+    let idBool = false;
+    let rez;
+
     workers.forEach((worker) => {
       if (worker.workerId == id) {
+        idBool = true;
+        rez = worker;
+      }
+
+      if (idBool) {
         res.writeHead(200, { "Content-Type": "application/json" });
-        return res.end(JSON.stringify(worker, null, 2));
+        return res.end(JSON.stringify(rez, null, 2));
+      } else {
+        res.writeHead(200, { "Content-Type": "application/json" });
+        return res.end(`WorkerId ${id} is not founded!!!`);
       }
     });
   } else {
@@ -124,114 +164,131 @@ function get_workers(res, id) {
   }
 }
 
-function check_keys(obj){
+function check_keys(obj) {
   let ret_obj = {
-    bool: true
-  }
-  for(let k in obj){
-    if(typeof(obj[k])=== 'string'){
-      obj[k] = obj[k].trim()
+    bool: true,
+  };
+  for (let k in obj) {
+    if (typeof obj[k] === "string") {
+      obj[k] = obj[k].trim();
     }
-    if(obj[k].length == 0){
-      ret_obj.bool = false
-      ret_obj['key'] = `${k}`
-    } 
-   }
-   return ret_obj  
+    if (obj[k].length == 0) {
+      ret_obj.bool = false;
+      ret_obj["key"] = `${k}`;
+    }
+  }
+  return ret_obj;
 }
 
-function post_file(res, nwData, fileName){
-  let chekByValue = check_keys(nwData) 
-  let idName = (fileName == 'branches'? fileName.slice(0,-2)+'Id': fileName.slice(0,-1)+'Id')
-  fileName = `${fileName}.json`
-  if(chekByValue.bool){
+function post_file(res, nwData, fileName) {
+  let chekByValue = check_keys(nwData);
+  let idName =
+    fileName == "branches"
+      ? fileName.slice(0, -2) + "Id"
+      : fileName.slice(0, -1) + "Id";
+  fileName = `${fileName}.json`;
+  if (chekByValue.bool) {
+    let allData = file_read(fileName);
+    let bool = true;
+    allData.forEach((data) => {
+      if ((data.title || data.name) == (nwData.title || nwData.name)) {
+        bool = false;
+      }
+    });
+    if (bool) {
+      allData[allData.length] = {};
+      allData[allData.length - 1][idName] = allData.length;
+      for (let i in nwData) {
+        allData[allData.length - 1][`${i}`] = nwData[`${i}`];
+      }
+      write_to_file(fileName, allData);
+      res.writeHead(200, { "Content-Type": "application/json" });
+      return res.end(`${idName.slice(0, -2)} was added!!!`);
+    } else {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      return res.end(`"${nwData.name || nwData.title}" is exists!`);
+    }
+  } else {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    return res.end(`${chekByValue.key} is not correct!`);
+  }
+}
+
+function update_files(res, nwData, fileName, id) {
+  let idName =
+    fileName == "branches"
+      ? fileName.slice(0, -2) + "Id"
+      : fileName.slice(0, -1) + "Id";
+  fileName = `${fileName}.json`;
   let allData = file_read(fileName);
-  let bool = true
-  allData.forEach(data => {
-      if((data.title || data.name) == (nwData.title || nwData.name)){
-        bool = false
-      } 
-  })
-  if(bool){
-    allData[allData.length] ={}
-    allData[allData.length-1][idName] = allData.length
-    for(let i in nwData){
-      allData[allData.length-1][`${i}`] = nwData[`${i}`]
+  let bool = true;
+
+  allData.forEach((data) => {
+    if (
+      (data.title || data.name) == (nwData.title || nwData.name) &&
+      data[idName] != id
+    ) {
+      bool = false;
     }
-    write_to_file(fileName, allData)
-    res.writeHead(200, { "Content-Type": "application/json" })
-    return res.end(`${idName.slice(0,-2)} was added!!!`)
-  }else{
-    res.writeHead(200, { "Content-Type": "application/json" })
-    return res.end(`"${nwData.name || nwData.title}" is exists!`)
-  }
-  }else{
-    res.writeHead(200, { "Content-Type": "application/json" })
-    return res.end(`${chekByValue.key} is not correct!`)
-  }
+  });
 
-}
+  if (bool) {
+    let idBool = false;
+    allData.forEach((data) => {
+      if (data[idName] == id) {
+        for (let k in data) {
+          data[k] = nwData[k] || data[k];
+          idBool = true;
+        }
+      }
+    });
 
-function update_files(res,nwData, fileName, id){
-  let idName = (fileName == 'branches'? fileName.slice(0,-2)+'Id': fileName.slice(0,-1)+'Id')
-  fileName = `${fileName}.json`
-  let allData = file_read(fileName)
-  let bool = true
-
-  allData.forEach(data => {
-
-    if(((data.title || data.name) == (nwData.title || nwData.name)) && (data[idName] != id)){
-      bool = false
-    } 
-})
-  
-  if(bool){
-
-    let idBool = false
-    allData.forEach(data => {
-      if(data[idName]== id){
-        for(let k in data){
-          data[k] = nwData[k] || data[k]
-          idBool = true
-        }      
-      }    
-    })
-    
-    if(!idBool){
-      res.writeHead(200, { "Content-Type": "application/json" })
-      return res.end(`${idName} ${id} is not founded!!`)   
+    if (!idBool) {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      return res.end(`${idName} ${id} is not founded!!`);
     }
 
-    write_to_file(fileName, allData)
-    res.writeHead(200, { "Content-Type": "application/json" })
-    return res.end(`${idName} ${id} is updated!!`)
-  
-  }else{
-
-    res.writeHead(200, { "Content-Type": "application/json" })
-    return res.end(`${nwData.title || nwData.name} is exists!!`)
-
+    write_to_file(fileName, allData);
+    res.writeHead(200, { "Content-Type": "application/json" });
+    return res.end(`${idName} ${id} is updated!!`);
+  } else {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    return res.end(`${nwData.title || nwData.name} is exists!!`);
   }
 }
 
-function delete_files(res, fileName, id){
-  let idName = (fileName == 'branches'? fileName.slice(0,-2)+'Id': fileName.slice(0,-1)+'Id')
-  fileName = `${fileName}.json`
-  let allData = file_read(fileName)
-  let bool = false
-  allData.forEach((data,inx) => {
-    if(data[idName]== id){
-      allData.splice(inx,1)
-      bool = true    
-    }    
-  })
-  if(bool){
-    write_to_file(fileName, allData)
-    res.writeHead(200, { "Content-Type": "application/json" })
-    return res.end(`${idName} ${id} is deleted!!`)
-  }else{
-    res.writeHead(200, { "Content-Type": "application/json" })
-    return res.end(`${idName} ${id} is not founded!!`)
+function delete_files(res, fileName, id) {
+  let idName =
+    fileName == "branches"
+      ? fileName.slice(0, -2) + "Id"
+      : fileName.slice(0, -1) + "Id";
+  fileName = `${fileName}.json`;
+  let allData = file_read(fileName);
+  let bool = false;
+  allData.forEach((data, inx) => {
+    if (data[idName] == id) {
+      allData.splice(inx, 1);
+      bool = true;
+    }
+  });
+  if (bool) {
+    write_to_file(fileName, allData);
+    res.writeHead(200, { "Content-Type": "application/json" });
+    return res.end(`${idName} ${id} is deleted!!`);
+  } else {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    return res.end(`${idName} ${id} is not founded!!`);
   }
 }
-export { file_read, update_files, delete_files, post_file, write_to_file, get_token, get_markets, get_branches, get_products, get_workers };
+export {
+  file_read,
+  update_files,
+  delete_files,
+  post_file,
+  write_to_file,
+  get_token,
+  get_markets,
+  get_branches,
+  get_products,
+  get_workers,
+};
--- 2-hafta/exam/src/server.js
+++ 2-hafta/exam/src/server.js
@@ -1,86 +1,78 @@
-import http from 'http'
-import { 
-    file_read,
-    get_token,
-    get_markets,
-    get_branches,
-    get_products,
-    get_workers,
-    post_file,
-    update_files,
-    delete_files
-} from './fs/fs_api.js'
-import url from 'url'
-import jwt from 'jsonwebtoken'
+import http from "http";
+import {
+  file_read,
+  get_token,
+  get_markets,
+  get_branches,
+  get_products,
+  get_workers,
+  post_file,
+  update_files,
+  delete_files,
+} from "./fs/fs_api.js";
+import url from "url";
+import jwt from "jsonwebtoken";
 
+let user = file_read("users.json");
 
-let user = file_read('users.json')
+http
+  .createServer(async (req, res) => {
+    let req_name = url.parse(req.url).pathname.split("/")[1];
+    let req_id = url.parse(req.url).pathname.split("/")[2];
 
-http .createServer(async(req,res) => {
-   let req_name = url.parse(req.url).pathname.split('/')[1] 
-   let req_id = url.parse(req.url).pathname.split('/')[2]
-
-
-   if(req.method == "POST"){
-    if(req_name == 'login'){
-        req.on('data', chunk => {
-            let user_data =JSON.parse(chunk)
-            get_token(user_data.name, user_data.password,res)
-        })
+    if (req.method == "POST") {
+      if (req_name == "login") {
+        req.on("data", (chunk) => {
+          let user_data = JSON.parse(chunk);
+          get_token(user_data.name, user_data.password, res);
+        });
+      }
     }
-        
-}
-if(req_name!='login'){    
-    try {
-        await jwt.verify(req.headers.authorization, process.env.SECRET_KEY)
-        
-        if(req.method == "GET"){
-            if(req_name == 'markets'){
-                get_markets(res,req_id)
+    if (req_name != "login") {
+      try {
+        await jwt.verify(req.headers.authorization, process.env.SECRET_KEY);
+
+        if (req.method == "GET") {
+          if (req_name == "markets") {
+            get_markets(res, req_id);
+          }
+
+          if (req_name == "branches") {
+            get_branches(res, req_id);
+          }
+
+          if (req_name == "products") {
+            get_products(res, req_id);
+          }
+
+          if (req_name == "workers") {
+            get_workers(res, req_id);
+          }
+        }
+
+        if (req.method == "POST") {
+          req.on("data", (chunk) => {
+            let newMarket = JSON.parse(chunk);
+            post_file(res, newMarket, req_name);
+          });
+        }
+
+        if (req.method == "PUT") {
+          req.on("data", (chunk) => {
+            let update_info = JSON.parse(chunk);
+            update_files(res, update_info, req_name, req_id);
+          });
+        }
+
+        if (req.method == "DELETE") {
+          delete_files(res, req_name, req_id);
+        }
+      } catch (error) {
+        res.writeHead(200, { "Content-Type": "application/json" });
+        return res.end("Token is not actual!!!");
+      }
     }
-
-    if(req_name == 'branches'){
-        get_branches(res,req_id)
-    }
-    
-    if(req_name == 'products'){
-        get_products(res,req_id)
-    }
-    
-    if(req_name == 'workers'){
-        get_workers(res,req_id)
-    }
-
-}
-
-
-if(req.method == 'POST'){  
-    
-    req.on('data', chunk => {
-        let newMarket = JSON.parse(chunk)
-        post_file(res,newMarket,req_name)    
-    })    
-}
-
-if (req.method == 'PUT'){
-    req.on('data', chunk => {
-            let update_info = JSON.parse(chunk)
-            update_files(res,update_info,req_name,req_id)
-        })
-    }
-    
-    if (req.method == 'DELETE'){
-        delete_files(res, req_name, req_id)
-    }
-
-    
-} catch (error) {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    return res.end('Token is not actual!!!');      
-}
-}
-
-
-}).listen(5555, ()=>{
+  })
+  .listen(5555, () => {
     console.log("server is running on 5555");
-})   
+  });
